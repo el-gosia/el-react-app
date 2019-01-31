d3e0db3d68bfa2ac9fff199081701f7c75cae158
Gosia <baksonek@gmail.com>
2019-01-31 23:51:03 +0100

    scaffolded & initially styled window sidebar

--- src/components/Window.js
+++ src/components/Window.js
@@ -2,4 +2,18 @@ import React from 'react';
 
 import './Window.scss';
 
-export const Window = () => <div className="window" />;
+const controlButtons = ['close', 'minimize', 'maximize'];
+
+export const Window = () => (
+  <div className="window">
+    <div className="sidebar">
+      <ul class="sidebar__controls">
+        {controlButtons.map(type => (
+          <li>
+            <button className={`sidebar__control sidebar__control--${type}`} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  </div>
+);

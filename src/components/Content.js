0d0d6a7241a149f362ea790028ea2135d1a867d6
Gosia <baksonek@gmail.com>
2019-02-02 17:49:17 +0100

    renamed color's name & entry --> record

--- src/components/Content.js
+++ src/components/Content.js
@@ -8,9 +8,9 @@ export const Content = ({ issues: register }) => (
   <div className="content">
     <ul className="register__list">
       {register.map(({ id, date, issues }) => (
-        <li key={id} className="entry">
-          <h2 className="entry__heading">{date}</h2>
-          <ul className="entry__issues-list">
+        <li key={id} className="record">
+          <h2 className="record__heading">{date}</h2>
+          <ul className="record__issues-list">
             {issues.map(({ name, open, ...issue }) => (
               <Issue key={issue.id} name={name} isOpen={open} />
             ))}

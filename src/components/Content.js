fd05c6518715ede4dc4787be68cc91135fbb6026
Gosia <baksonek@gmail.com>
2019-02-02 12:12:40 +0100

    Content component scaffold

---
+++ src/components/Content.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './Content.scss';
+
+export const Content = () => (
+  <div className="content">
+    <ul className="register__list" />
+  </div>
+);
--- src/components/Window.js
+++ src/components/Window.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
 import { Sidebar } from './Sidebar';
+import { Content } from './Content';
 
 import './Window.scss';
 
 export const Window = () => (
   <div className="window">
     <Sidebar />
-    <div className="content" />
+    <Content />
   </div>
 );

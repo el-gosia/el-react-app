b70f9bc1bd70697b6983151e46dcf30016e64818
Gosia <baksonek@gmail.com>
2019-02-02 15:41:24 +0100

    fetched issues from the mock register & passed it to Window component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Loader from 'react-loader';
 
 import { Window } from './components/Window';
 
 import './App.scss';
 
-export const App = () => (
-  <div className="app">
-    <Window />
-  </div>
-);
+export class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true,
+      issues: [],
+    };
+  }
+
+  async componentDidMount() {
+    const response = await fetch('http://localhost:9000/register');
+    const issues = await response.json();
+    this.setState({ issues, isLoading: false });
+  }
+
+  render() {
+    const { isLoading, issues } = this.state;
+
+    if (isLoading) return <Loader />;
+    return (
+      <div className="app">
+        <Window issues={issues} />
+      </div>
+    );
+  }
+}

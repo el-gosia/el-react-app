007cac98a77b4bd25cadcd2b48bfdeb507ba6282
Gosia <baksonek@gmail.com>
2019-02-03 16:21:38 +0100

    replaced pure css at-rule-no-unknown rule with an scss equivalent

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,3 +1,8 @@
 module.exports = {
   extends: ['stylelint-config-recommended', 'stylelint-config-prettier'],
+  plugins: ['stylelint-scss'],
+  rules: {
+    'at-rule-no-unknown': null,
+    'scss/at-rule-no-unknown': true,
+  },
 };

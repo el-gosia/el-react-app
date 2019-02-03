b45532576f508c7f7afcf02e021536f2003c4576
Gosia <baksonek@gmail.com>
2019-02-03 17:47:46 +0100

    Updates

---
+++ precache-manifest.5ee1a6004edd651ee8f7e82af738f213.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "3b1c8430e00a12c306ddc5402aa42183",
+    "url": "/el-react-app/static/media/icon-star.3b1c8430.svg"
+  },
+  {
+    "revision": "8e9b9760b6611145e8809330ab09b1f3",
+    "url": "/el-react-app/static/media/icon-open-issue.8e9b9760.svg"
+  },
+  {
+    "revision": "29b3814a2c930df4e12209af5a257400",
+    "url": "/el-react-app/static/media/icon-github.29b3814a.svg"
+  },
+  {
+    "revision": "48aafe2c70a3e965d62eb329c41a69c2",
+    "url": "/el-react-app/static/media/icon-closed-issue.48aafe2c.svg"
+  },
+  {
+    "revision": "e22ac107747402cfb9937174a860a3dd",
+    "url": "/el-react-app/static/media/background.e22ac107.jpg"
+  },
+  {
+    "revision": "319cd467658ad829d5cd",
+    "url": "/el-react-app/static/js/runtime~main.319cd467.js"
+  },
+  {
+    "revision": "d2a7339ad5666275606f",
+    "url": "/el-react-app/static/js/main.d2a7339a.chunk.js"
+  },
+  {
+    "revision": "da599b756d768efee7d3",
+    "url": "/el-react-app/static/js/1.da599b75.chunk.js"
+  },
+  {
+    "revision": "d2a7339ad5666275606f",
+    "url": "/el-react-app/static/css/main.a5e53783.chunk.css"
+  },
+  {
+    "revision": "c65696ed86fed2d102baea0be19059d3",
+    "url": "/el-react-app/index.html"
+  }
+];
